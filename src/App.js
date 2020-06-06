c2be606da9af441a7fe1f00330d311487eb30d57
Tomas <tomas.milkintas1@gmail.com>
2020-06-06 16:55:25 +0100

    setting things up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>New App</h1>
+            <p>Clean Layout!</p>
+        </div>
+    );
+}
+
+export default App;
